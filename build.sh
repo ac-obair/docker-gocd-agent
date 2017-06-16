82e050b3bf38afb2bb7fa6ca57733b04360868cb
Jason Leitch <jason.leitch@arnoldclark.com>
2017-06-16 15:20:23 +0100

    adding docker 1.13.1 and removing unnecessary CMD line

--- build.sh
+++ build.sh
@@ -17,3 +17,7 @@ GOCD_FULL_VERSION=17.5.0-5095 \
 GOCD_GIT_SHA=ce5f115d1dc008fab6166ad220772949114a875f \
 GOCD_AGENT_DOWNLOAD_URL=https://download.gocd.io/binaries/17.5.0-5095/generic/go-agent-17.5.0-5095.zip \
 rake gocd-agent-alpine-3.5:build_image
+
+# Grab new image ID and tag for testing
+IMAGEID=$(docker image ls gocd-agent-alpine-3.5 -q)
+docker tag $IMAGEID devregistry.arnoldclark.com/gocd/docker-ansible-agent-alpine-3.5:v17.5.0
