5cc4e5e4232ae3292d3d1161795003c15d4b65b3
Jason Leitch <jason.leitch@arnoldclark.com>
2017-06-23 10:46:47 +0100

    added auth for secure docker api

--- get_previous.py
+++ get_previous.py
@@ -5,15 +5,18 @@ import sys
 import os
 
 
-
 def get_previous(service_name):
+
 
     with open(service_name, 'r') as f:
         pipeline = f.read().strip('\n')
-
+     
     url = "https://10.150.232.234:8154/go/api/pipelines/{pipeline}/history".format(pipeline=pipeline)
-    r = requests.get(url, verify=False).json()
-    return r["pipelines"][1]["build_cause"]["material_revisions"][0]["modifications"][0]["revision"][:7]
+    print(url)
+    req = requests.Session()
+    req.auth = ("agent", os.environ["AGENT_PASSWORD"])
+    result = req.get(url, verify=False).json()
+    return result["pipelines"][1]["build_cause"]["material_revisions"][0]["modifications"][0]["revision"][:7]
 
 
 if __name__ == "__main__":
