2081b0fe7c2bae0344018fdddf4939c3d7e1c9d9
Jason Leitch <jason.leitch@arnoldclark.com>
2017-06-27 10:11:31 +0100

    building out rollback program

--- rollback.py
+++ rollback.py
@@ -8,33 +8,61 @@ from pprint import pprint as pp
 
 def get_pipeline_name(f):
 
-    with open(f, 'r') as service_name:
-        return service_name.read().strip('\n')
+    # Either read the name from a file or stdin 
+    # Note file seem to work best within the pipeline call
 
+    try:
+        with open(f, 'r') as service_name:
+            return service_name.read().strip('\n')
+    except TypeError:
+        return f
+    except FileNotFoundError:
+        return f
 
 def fetch_pipeline_data(service_name):
+ 
+    # Build a url with with name of the pipeline you wish to query
 
     url = "https://10.150.232.234:8154/go/api/pipelines/{pipeline}/history".format(pipeline=service_name)
+    print(url) 
     # sessions persist meta data for a connection across calls
     req = requests.Session()
-    req.auth = ("agent", os.environ["AGENT_PASSWORD"])
-    #req.auth = ("lighiche", "")
-
+    #req.auth = ("agent", os.environ["AGENT_PASSWORD"])
+    req.auth = ("lighiche", os.environ["MY_PASS"])
     result = req.get(url, verify=False).json()
  
     return return_last_successful_deploy(result)
 
+def check_pipeline_type(result):
+
+    # Pipelines with git material use SHA as their revision.
+    # Whereas deployment pipelines depend on material from other pipelines
+    # These use pipelines/label/stage/counter for revision. We must parse this out and rerun the request.
+
+    # This logic is implemented to keep functionality for parsing both types of material name 
+    # Following CI/CD logic the last successfully build image should've been the last to run in prod
+   
+    check_material = result["pipelines"][0]["build_cause"]["material_revisions"][0]["modifications"][0]["revision"]
+
+    try:
+        return fetch_pipeline_data(check_material[:check_material.index('/')])
+    except ValueError:
+        # modify nothing if there's no / in the revision name
+        return result
+
+
 
 def return_last_successful_deploy(result): 
-
+    result = check_pipeline_type(result)
+    
     # history for 10 pipelines kept
     for index in range(1, len(result["pipelines"])):
-
         # Get label and release revision for last 10 deploys, if new pipeline IndexError is caught
         revision = result["pipelines"][index]["build_cause"]["material_revisions"][0]["modifications"][0]["revision"]
+      
         label = int(result["pipelines"][index]["label"])
 
-        print("Checking for last successful deploy:  stages for label {label} on {rev}".format(label=label, rev=revision[:7]))
+        print("Checking for last successful deploy:  stages for label {label} on {rev}".format(label=label, rev=revision))
 
         # Parse number of stages for last 10 deploys, if new pipeline IndexError is caught
         stage_len = len(result["pipelines"][index]["stages"])
