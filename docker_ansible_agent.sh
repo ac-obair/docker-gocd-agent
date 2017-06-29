8e32c7f4f654df3160df10f74b225b7cdb665bd1
Jason Leitch <jason.leitch@arnoldclark.com>
2017-06-29 13:55:47 +0100

    updated build script to use image 17.7.0; release notes are security fixes for 17.5.0 and 17.6.0

--- docker_ansible_agent.sh
+++ docker_ansible_agent.sh
@@ -3,8 +3,8 @@
 : <<COMMENT
 to list versions you can build:
 
-GOCD_VERSION=17.5.0 \
-GOCD_FULL_VERSION=17.5.0-5095 \
+GOCD_VERSION=17.7.0 \
+GOCD_FULL_VERSION=17.7.0-5095 \
 GOCD_GIT_SHA=ce5f115d1dc008fab6166ad220772949114a875f \
 GOCD_AGENT_DOWNLOAD_URL=https://download.gocd.io/binaries/17.5.0-5095/generic/go-agent-17.5.0-5095.zip \
 rake -T build_image
@@ -12,12 +12,12 @@ COMMENT
 
 
 # current latest version over alpine 
-GOCD_VERSION=17.5.0 \
-GOCD_FULL_VERSION=17.5.0-5095 \
+GOCD_VERSION=17.7.0 \
+GOCD_FULL_VERSION=17.7.0-5095 \
 GOCD_GIT_SHA=ce5f115d1dc008fab6166ad220772949114a875f \
-GOCD_AGENT_DOWNLOAD_URL=https://download.gocd.io/binaries/17.5.0-5095/generic/go-agent-17.5.0-5095.zip \
+GOCD_AGENT_DOWNLOAD_URL=https://download.gocd.io/binaries/17.7.0-5147/generic/go-agent-17.7.0-5147.zip \
 rake gocd-agent-alpine-3.5:build_image
 
 # Grab new image ID and tag for testing
 IMAGEID=$(docker image ls gocd-agent-alpine-3.5 -q)
-docker tag $IMAGEID devregistry.arnoldclark.com/gocd/docker-ansible-agent-alpine-3.5:v17.5.0
+docker tag $IMAGEID devregistry.arnoldclark.com/gocd/docker-ansible-agent-alpine-3.5:v17.7.0
