33a15d6a3151ca7327ed2d1cd3f49ab3b1e55ce1
Jason Leitch <jason.leitch@arnoldclark.com>
2017-06-29 14:28:44 +0100

    added better way to grab new image ids for tagging

--- docker_ansible_agent.sh
+++ docker_ansible_agent.sh
@@ -8,6 +8,10 @@ GOCD_FULL_VERSION=17.7.0-5095 \
 GOCD_GIT_SHA=ce5f115d1dc008fab6166ad220772949114a875f \
 GOCD_AGENT_DOWNLOAD_URL=https://download.gocd.io/binaries/17.5.0-5095/generic/go-agent-17.5.0-5095.zip \
 rake -T build_image
+
+To upgrade the agent replace all instances over 17.7.0 with the new version. 
+
+Note the build version 0-5147 can be gotten from the gocd master server under the bell icon letting you know that theres a new release.
 COMMENT
 
 
@@ -19,5 +23,5 @@ GOCD_AGENT_DOWNLOAD_URL=https://download.gocd.io/binaries/17.7.0-5147/generic/go
 rake gocd-agent-alpine-3.5:build_image
 
 # Grab new image ID and tag for testing
-IMAGEID=$(docker image ls gocd-agent-alpine-3.5 -q)
+IMAGEID=$(docker image ls --filter reference='*:v17.7.0' -q)
 docker tag $IMAGEID devregistry.arnoldclark.com/gocd/docker-ansible-agent-alpine-3.5:v17.7.0
