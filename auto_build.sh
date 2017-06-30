dc0c41bd9ddf846539d4be22056709f5c039f568
Jason Leitch <jason.leitch@arnoldclark.com>
2017-06-30 14:17:42 +0100

    exporting values to parent shell

--- auto_build.sh
+++ auto_build.sh
@@ -14,14 +14,18 @@ To upgrade the agent replace all instances over 17.7.0 with the new version.
 Note the build version 0-5147 can be gotten from the gocd master server under the bell icon letting you know that theres a new release.
 COMMENT
 
-
 # current latest version over alpine 
 GOCD_VERSION=17.7.0 \
 GOCD_FULL_VERSION=17.7.0-5095 \
 GOCD_GIT_SHA=ce5f115d1dc008fab6166ad220772949114a875f \
 GOCD_AGENT_DOWNLOAD_URL=https://download.gocd.io/binaries/17.7.0-5147/generic/go-agent-17.7.0-5147.zip \
-/usr/bin/rake gocd-agent-alpine-3.5:build_image
+rake gocd-agent-alpine-3.5:build_image
 
 # Grab new image ID and tag for test
-echo $GOCD_VERSION > VERSION.txt
-DOCKER_HOST="tcp://mrlswarm-bld01:2375" docker image ls --filter reference="*:$GOCD_VERSION" -q > IMAGEID.txt
+
+#DOCKER_HOST="tcp://mrlswarm-bld01:2375" docker image ls --filter reference="*:$GOCD_VERSION" -q > IMAGEID.txt
+IMAGEID=$(DOCKER_HOST="tcp://mrlswarm-bld01:2375" docker image ls --filter reference="*:$GOCD_VERSION" -q)
+
+
+export IMAGEID
+export GOCD_VERSION
