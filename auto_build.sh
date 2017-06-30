e5cfcc57c864f0b1a71c13cb33f7d35566e3b59b
Jason Leitch <jason.leitch@arnoldclark.com>
2017-06-30 13:26:37 +0100

    moving tag funcationality into task on gocd, and updated image id grab to file instead of var

--- auto_build.sh
+++ auto_build.sh
@@ -22,6 +22,5 @@ GOCD_GIT_SHA=ce5f115d1dc008fab6166ad220772949114a875f \
 GOCD_AGENT_DOWNLOAD_URL=https://download.gocd.io/binaries/17.7.0-5147/generic/go-agent-17.7.0-5147.zip \
 /usr/bin/rake gocd-agent-alpine-3.5:build_image
 
-# Grab new image ID and tag for testing
-IMAGEID=$(docker image ls --filter reference='*:v17.7.0' -q)
-docker tag $IMAGEID devregistry.arnoldclark.com/gocd/docker-ansible-agent-alpine-3.5:v17.7.0
+# Grab new image ID and tag for test
+DOCKER_HOST="tcp://mrlswarm-bld01:2375" docker image ls --filter reference='*:v17.7.0' -q > IMAGEID.txt
